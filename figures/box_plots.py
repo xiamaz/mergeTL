dc5a518df163d890352d1e2e76beaf85ffcdaacf
NandithaMallesh <nanditha.mallesh@gmail.com>
2021-02-01 14:13:10 +0100

    add base model perf to box plots

--- figures/box_plots.py
+++ figures/box_plots.py
@@ -1,22 +1,24 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
 
 
 def create_boxplot():
+    # path to excel with F1 scores from k-fold run
     df = pd.read_excel("F1_scores_k-fold.xlsx")
 
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
     plt.style.use("PaperDoubleFig.mplstyle")
 
     g1 = sns.boxplot(x="Dataset", y="Weighted_F1_score", hue="Protocol", data=df, showfliers=False, ax=axes[0])
+    g1.axhline(0.937, ls='--')  # base model weighted F1 score
     g1.set(title='weighted_F1 score')  # add a title
     g1.set(xlabel=None)  # remove the axis label
     g1.set(ylabel=None)
 
     plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     g2 = sns.boxplot(x="Dataset", y="Avg_F1_score", hue="Protocol", data=df, showfliers=False, ax=axes[1])
+    g2.axhline(0.76, ls='--')  # base model avg F1 score
     g2.set(title='average_F1 score')  # add a title
     g2.set(xlabel=None)  # remove the axis label
     g2.set(ylabel=None)
